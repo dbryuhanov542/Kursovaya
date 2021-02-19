adc4726983ae39cda0c110a22e2836fa9a417558
dbryuhanov542 <d.bryuhanov542@yandex.ru>
2021-02-19 10:21:40 +0400

    сохранения настроек

---
+++ Kursovaya/Classes/Functions.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Kursovaya
+{
+    //Класс, содержащий методы функций, вызываемые в основной программе
+    class Functions
+    {
+        //Функция
+        public static DataTable Connect(string a)
+        {
+            DataTable dt = new DataTable();
+            MySqlConnectionStringBuilder mysqlconstr = new MySqlConnectionStringBuilder
+            {
+                Server = "127.0.0.1",
+                Port = 3306,
+                Database = "KursovayaDB",
+                UserID = "root",
+                Password = "",
+                CharacterSet = "utf8"
+            };
+            MySqlConnection connect = new MySqlConnection(mysqlconstr.ToString());
+            string queryString = @a;
+            MySqlCommand command = new MySqlCommand(queryString, connect);
+            try
+            {
+                connect.Open();
+
+                MySqlDataReader dr = command.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    dt.Load(dr);
+                }
+                connect.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Операция не удалась /n Ошибка: " + ex.Message);
+            }
+
+            return dt;
+        }
+
+
+        static public DataTable Add(string comm, string req_type, string cl_name, string ord_date, string date_end, string price, string comment)
+        {
+            DataTable dt = new DataTable();
+            MySqlConnectionStringBuilder mysqlconstr = new MySqlConnectionStringBuilder
+            {
+                Server = "127.0.0.1",
+                Port = 3306,
+                Database = "KursovayaDB",
+                UserID = "root",
+                Password = "",
+                CharacterSet = "utf8"
+            };
+            MySqlConnection connect = new MySqlConnection(mysqlconstr.ToString());
+            string queryString = @comm;
+            MySqlCommand command = new MySqlCommand(queryString, connect);
+            command.Parameters.AddWithValue("@request_name", req_type);
+            command.Parameters.AddWithValue("@client_name", cl_name);
+            command.Parameters.AddWithValue("@data_zakaza", ord_date);
+            command.Parameters.AddWithValue("@date_end", date_end);
+            command.Parameters.AddWithValue("@price", price);
+            command.Parameters.AddWithValue("@comment", comment);
+            connect.Open();
+            try
+            {
+                command.ExecuteNonQuery();
+                MessageBox.Show("Заявка успешно зарегистрирована.");
+                connect.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("555Сохранение заявки не удалось /n Ошибка: " + ex.Message);
+            }
+            return dt;
+        }
+    }
+}
--- Kursovaya/Forms/DBset_form.Designer.cs
+++ Kursovaya/Forms/DBset_form.Designer.cs
@@ -38,20 +38,33 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.panel3 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.label7 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.panel4 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(114, 25);
+            this.textBox1.Location = new System.Drawing.Point(114, 46);
             this.textBox1.Margin = new System.Windows.Forms.Padding(7);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(225, 20);
@@ -59,7 +72,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(114, 59);
+            this.textBox2.Location = new System.Drawing.Point(114, 80);
             this.textBox2.Margin = new System.Windows.Forms.Padding(7);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(225, 20);
@@ -67,7 +80,7 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(114, 93);
+            this.textBox3.Location = new System.Drawing.Point(114, 114);
             this.textBox3.Margin = new System.Windows.Forms.Padding(7);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(225, 20);
@@ -75,7 +88,7 @@
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(114, 127);
+            this.textBox4.Location = new System.Drawing.Point(114, 148);
             this.textBox4.Margin = new System.Windows.Forms.Padding(7);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(225, 20);
@@ -83,7 +96,7 @@
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(114, 161);
+            this.textBox5.Location = new System.Drawing.Point(114, 182);
             this.textBox5.Margin = new System.Windows.Forms.Padding(7);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(225, 20);
@@ -131,6 +144,7 @@
             // panel3
             // 
             this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Controls.Add(this.button2);
             this.panel3.Controls.Add(this.button1);
             this.panel3.Controls.Add(this.label7);
             this.panel3.Controls.Add(this.label6);
@@ -147,10 +161,30 @@
             this.panel3.Size = new System.Drawing.Size(364, 262);
             this.panel3.TabIndex = 9;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(113, 218);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Вернуться";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(229, 218);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Подключиться ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(59, 164);
+            this.label7.Location = new System.Drawing.Point(59, 185);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(45, 13);
             this.label7.TabIndex = 9;
@@ -159,7 +193,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(66, 130);
+            this.label6.Location = new System.Drawing.Point(66, 151);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(38, 13);
             this.label6.TabIndex = 8;
@@ -168,7 +202,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(6, 96);
+            this.label5.Location = new System.Drawing.Point(6, 117);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(98, 13);
             this.label5.TabIndex = 7;
@@ -177,7 +211,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(31, 62);
+            this.label4.Location = new System.Drawing.Point(31, 83);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(73, 13);
             this.label4.TabIndex = 6;
@@ -186,7 +220,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(21, 28);
+            this.label3.Location = new System.Drawing.Point(21, 49);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(83, 13);
             this.label3.TabIndex = 5;
@@ -195,20 +229,116 @@
             // panel4
             // 
             this.panel4.BackColor = System.Drawing.Color.White;
+            this.panel4.Controls.Add(this.label14);
+            this.panel4.Controls.Add(this.label15);
+            this.panel4.Controls.Add(this.label16);
+            this.panel4.Controls.Add(this.label17);
+            this.panel4.Controls.Add(this.label18);
+            this.panel4.Controls.Add(this.label13);
+            this.panel4.Controls.Add(this.label12);
+            this.panel4.Controls.Add(this.label11);
+            this.panel4.Controls.Add(this.label10);
+            this.panel4.Controls.Add(this.label9);
+            this.panel4.Controls.Add(this.label8);
             this.panel4.Location = new System.Drawing.Point(395, 109);
             this.panel4.Name = "panel4";
             this.panel4.Size = new System.Drawing.Size(280, 262);
             this.panel4.TabIndex = 10;
             // 
-            // button1
+            // label14
             // 
-            this.button1.Location = new System.Drawing.Point(229, 208);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(110, 23);
-            this.button1.TabIndex = 10;
-            this.button1.Text = "Подключиться ";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(84, 185);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(0, 13);
+            this.label14.TabIndex = 10;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(84, 151);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(0, 13);
+            this.label15.TabIndex = 9;
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(84, 117);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(0, 13);
+            this.label16.TabIndex = 8;
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(84, 83);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(0, 13);
+            this.label17.TabIndex = 7;
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(84, 49);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(0, 13);
+            this.label18.TabIndex = 6;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label13.Location = new System.Drawing.Point(3, 11);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(187, 32);
+            this.label13.TabIndex = 5;
+            this.label13.Text = "Информация о текущем \r\nподключении";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(30, 185);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(48, 13);
+            this.label12.TabIndex = 4;
+            this.label12.Text = "Пароль:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(37, 151);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(41, 13);
+            this.label11.TabIndex = 3;
+            this.label11.Text = "Логин:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(3, 117);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(75, 13);
+            this.label10.TabIndex = 2;
+            this.label10.Text = "База данных:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(43, 83);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "Порт:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(31, 49);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(47, 13);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Сервер:";
             // 
             // DBset_form
             // 
@@ -227,6 +357,8 @@
             this.panel1.PerformLayout();
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -250,5 +382,17 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label18;
     }
 }
